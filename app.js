acb5276986dcc0c71173609624adce95847594c2
Raul <UO265210@uniovi.es>
2021-04-11 23:46:53 +0200

    SDI-2021-502-9.6-Manejo de errores y HTTPS.

--- app.js
+++ app.js
@@ -2,6 +2,9 @@
 let express = require('express');
 let app = express();
 
+let fs = require('fs');
+let https = require('https');
+
 let expressSession = require('express-session');
 app.use(expressSession({
     secret: 'abcdefg',
@@ -17,47 +20,69 @@ let swig = require('swig');
 
 let bodyParser = require('body-parser');
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
 
 let gestorBD = require("./modules/gestorBD.js");
-gestorBD.init(app,mongo);
+gestorBD.init(app, mongo);
 
 // routerUsuarioSession
 var routerUsuarioSession = express.Router();
-routerUsuarioSession.use(function(req, res, next) {
+routerUsuarioSession.use(function (req, res, next) {
     console.log("routerUsuarioSession");
-    if ( req.session.usuario ) {
+    if (req.session.usuario) {
         // dejamos correr la petición
         next();
     } else {
-        console.log("va a : "+req.session.destino)
+        console.log("va a : " + req.session.destino)
         res.redirect("/identificarse");
     }
 });
 //Aplicar routerUsuarioSession
-app.use("/canciones/agregar",routerUsuarioSession);
-app.use("/publicaciones",routerUsuarioSession);
-app.use("/cancion/comprar",routerUsuarioSession);
-app.use("/compras",routerUsuarioSession);
+app.use("/canciones/agregar", routerUsuarioSession);
+app.use("/publicaciones", routerUsuarioSession);
+app.use("/cancion/comprar", routerUsuarioSession);
+app.use("/compras", routerUsuarioSession);
+
+//routerUsuarioAutor
+let routerUsuarioAutor = express.Router();
+routerUsuarioAutor.use(function (req, res, next) {
+    console.log("routerUsuarioAutor");
+    let path = require('path');
+    let id = path.basename(req.originalUrl);
+// Cuidado porque req.params no funciona
+// en el router si los params van en la URL.
+    gestorBD.obtenerCanciones(
+        {_id: mongo.ObjectID(id)}, function (canciones) {
+            console.log(canciones[0]);
+            if (canciones[0].autor == req.session.usuario) {
+                next();
+            } else {
+                res.redirect("/tienda");
+            }
+        })
+});
+//Aplicar routerUsuarioAutor
+app.use("/cancion/modificar", routerUsuarioAutor);
+app.use("/cancion/eliminar", routerUsuarioAutor);
 
 //routerAudios
 let routerAudios = express.Router();
-routerAudios.use(function(req, res, next) {
+routerAudios.use(function (req, res, next) {
     console.log("routerAudios");
     let path = require('path');
     let idCancion = path.basename(req.originalUrl, '.mp3');
     gestorBD.obtenerCanciones(
-        {"_id": mongo.ObjectID(idCancion) }, function (canciones) {
-            if(req.session.usuario && canciones[0].autor == req.session.usuario ){
+        {"_id": mongo.ObjectID(idCancion)}, function (canciones) {
+            if (req.session.usuario && canciones[0].autor == req.session.usuario) {
                 next();
             } else {
                 let criterio = {
-                    usuario : req.session.usuario,
-                    cancionId : mongo.ObjectID(idCancion)
+                    usuario: req.session.usuario,
+                    cancionId: mongo.ObjectID(idCancion)
                 };
 
-                gestorBD.obtenerCompras(criterio ,function(compras){
-                    if (compras != null && compras.length > 0 ){
+                gestorBD.obtenerCompras(criterio, function (compras) {
+                    if (compras != null && compras.length > 0) {
                         next();
                     } else {
                         res.redirect("/tienda");
@@ -67,32 +92,10 @@ routerAudios.use(function(req, res, next) {
         })
 });
 //Aplicar routerAudios
-app.use("/audios/",routerAudios);
-
-//routerUsuarioAutor
-let routerUsuarioAutor = express.Router();
-routerUsuarioAutor.use(function(req, res, next) {
-    console.log("routerUsuarioAutor");
-    let path = require('path');
-    let id = path.basename(req.originalUrl);
-// Cuidado porque req.params no funciona
-// en el router si los params van en la URL.
-    gestorBD.obtenerCanciones(
-        {_id: mongo.ObjectID(id) }, function (canciones) {
-            console.log(canciones[0]);
-            if(canciones[0].autor == req.session.usuario ){
-                next();
-            } else {
-                res.redirect("/tienda");
-            }
-        })
-});
-//Aplicar routerUsuarioAutor
-app.use("/cancion/modificar",routerUsuarioAutor);
-app.use("/cancion/eliminar",routerUsuarioAutor);
+app.use("/audios/", routerAudios);
 
 let routerComentarios = express.Router();
-routerComentarios.use(function(req, res, next) {
+routerComentarios.use(function (req, res, next) {
     console.log("routerComentarios");
 
     if (req.session.usuario) {
@@ -103,7 +106,7 @@ routerComentarios.use(function(req, res, next) {
 
 });
 //Aplicar routerAudios
-app.use("/comentarios/:cancion_id",routerComentarios);
+app.use("/comentarios/:cancion_id", routerComentarios);
 
 
 app.use(express.static('public'));
@@ -111,8 +114,8 @@ app.use(express.static('public'));
 //Variables
 app.set('port', 8081);
 app.set('db', 'mongodb://admin:Lwvvid17@tiendamusica-shard-00-00.hzqer.mongodb.net:27017,tiendamusica-shard-00-01.hzqer.mongodb.net:27017,tiendamusica-shard-00-02.hzqer.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-sq6wyx-shard-0&authSource=admin&retryWrites=true&w=majority');
-app.set('clave','abcdefg');
-app.set('crypto',crypto);
+app.set('clave', 'abcdefg');
+app.set('crypto', crypto);
 
 //Rutas/controladores por lógica
 require("./routes/rusuarios.js")(app, swig, gestorBD);
@@ -124,7 +127,18 @@ app.get('/', function (req, res) {
     res.redirect('/tienda');
 });
 
+app.use(function (err, req, res, next) {
+    console.log("Error producido: " + err);
+    if (!res.headersSent) {
+        res.status(400);
+        res.send("Recurso no disponible");
+    }
+});
+
 //Lanzar el sevidor
-app.listen(app.get('port'), function(){
-    console.log('Servidor activo');
+https.createServer({
+    key: fs.readFileSync('certificates/alice.key'),
+    cert: fs.readFileSync('certificates/alice.crt')
+}, app).listen(app.get('port'), function () {
+    console.log("Servidor activo");
 });
